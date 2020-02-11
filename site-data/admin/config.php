e9b06b50873e7aa0d59d6bd22546125d1c4b3dd2
moosavi.smd <moosavi.smd@arvancloud.com>
2020-02-11 16:45:40 +0330

    first commit

---
+++ site-data/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/admin/');
+define('HTTP_CATALOG', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/admin/');
+define('HTTPS_CATALOG', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/admin/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_CATALOG', '/var/www/html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql-svc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'notsecured');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ek_');
---
+++ site-data/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://online-shop-example-project.apps.ir-thr-mn1.arvan.run/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/catalog/');
+define('DIR_SYSTEM', '/var/www/html/system/');
+define('DIR_LANGUAGE', '/var/www/html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/system/config/');
+define('DIR_IMAGE', '/var/www/html/image/');
+define('DIR_CACHE', '/var/www/html/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/system/download/');
+define('DIR_UPLOAD', '/var/www/html/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/system/modification/');
+define('DIR_LOGS', '/var/www/html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql-svc');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'notsecured');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ek_');
